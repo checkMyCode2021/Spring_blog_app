982a1d9c2405d85cde807c787ee3432532ea27e6
Marek Grota <CheckMyCode2021@gmail.com>
2022-02-08 22:17:25 +0100

    User class added

--- src/main/java/com/example/spring_blog_app/model/User.java
+++ src/main/java/com/example/spring_blog_app/model/User.java
@@ -1,4 +1,20 @@
 package com.example.spring_blog_app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private int userId;
+    private String email;
+    private String password;
+    private LocalDateTime registrationDateTime;
+    private boolean status;
 }
