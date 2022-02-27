d56a95fc17973ea33aa4800e7bb0375aae24b14f
Marek Grota <CheckMyCode2021@gmail.com>
2022-02-27 13:51:12 +0100

    adding posts functionality added

--- src/main/java/com/example/spring_blog_app/controller/BlogController.java
+++ src/main/java/com/example/spring_blog_app/controller/BlogController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -50,4 +52,11 @@ public class BlogController {
         return "addPost";
     }
 
+    @PostMapping("addPost")
+    public String addPost(
+            @ModelAttribute Post post
+    ){
+        postService.addPost(post.getTitle(), post.getContent(),post.getCategory(),userService.getUserById(1).get());
+        return "redirect:/";
+    }
 }
