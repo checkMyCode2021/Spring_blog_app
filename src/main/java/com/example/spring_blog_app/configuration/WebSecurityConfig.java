560c77ac4afc976f199c6237a717b9bccab72d85
Marek Grota <CheckMyCode2021@gmail.com>
2022-03-09 22:11:43 +0100

    Login users from bd functionality added

--- src/main/java/com/example/spring_blog_app/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/spring_blog_app/configuration/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.spring_blog_app.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
@@ -27,5 +32,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/");
+    }
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private EncoderAlgorithm encoderAlgorithm;
+    protected void  configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("SELECT u.email, u.password, u.status FROM user u WHERE u.email = ?")
+                .authoritiesByUsernameQuery("SELECT u.email, r.role_name FROM user u JOIN user_roles ur ON " +
+                        "(u.user_id = ur.user_user_id) JOIN role r ON (r.role_id = ur.roles_role_id) WHERE u.email = ?")
+                .dataSource(dataSource)
+                .passwordEncoder(encoderAlgorithm.getPasswordEncoder());
     }
 }
